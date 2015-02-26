72fe7b9784ea2316db821fca0127b7bb5bcf05cf
ivannalb <ivanna.96@hotmail.com>
2015-02-26 08:43:44 -0600

    :)

---
+++ funwithnumbers.cpp
@@ -0,0 +1,21 @@
+//funwithnumbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+	cout <<"give a value";
+	cin >> x;
+	cout <<"give another value:";
+	cin >> y;
+	cout <<"the sum of two number is: " << x+y << endl;
+	cout <<"the diference of two numbers is: " << x-y << endl;
+	cout <<"the product of two numbers is: " << x*y << endl;
+	cout <<"the division of two numbers is: "<< x/y << endl;
+	cout <<"the remainder of the integer division of two numbers is: " << x%y << endl;
+
+	 return 0;
+}
